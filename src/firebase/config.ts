ef363478fb630108e521164df6ca69a56691d9e3
AdBoostra <sqrbofficial@gmail.com>
2026-01-01 19:31:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1637293473-94b46",
+  "appId": "1:543166265984:web:25d49732d64741cdd2c465",
+  "apiKey": "AIzaSyCUGzRggJ0Jmt7DVoyqkdUJS7wVgNnto4I",
+  "authDomain": "studio-1637293473-94b46.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "543166265984"
+};
