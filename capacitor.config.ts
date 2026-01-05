54a2e6c9d5f2b7d8ba8d84fbc8af9bf7cddb879c
AdBoostra <sqrbofficial@gmail.com>
2026-01-05 13:20:14 +0000

    Next steps: https://capacitorjs.com/docs/getting-started#where-to-go-ne

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ariscbt.com',
+  appName: 'ArisCBT',
+  webDir: 'out'
+};
+
+export default config;
