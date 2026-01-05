e481706a471e5d65a9d3d5307beb435dca9986e3
AdBoostra <sqrbofficial@gmail.com>
2026-01-05 13:50:55 +0000

    '/home/user/studio/android/app/src/main/assets/capacitor.plugins.json' s

--- android/app/src/main/java/com/ariscbt/com/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.ariscbt.com;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,9 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.ariscbt.com',
   appName: 'ArisCBT',
   webDir: 'out',
-  server: {
-    url: 'https://ariscbt.vercel.app/',
-  }
 };
 
 export default config;
