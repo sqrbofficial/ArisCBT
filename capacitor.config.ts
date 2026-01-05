533d21e0c5f52c8e5578153ab366317956270ebb
AdBoostra <sqrbofficial@gmail.com>
2026-01-05 13:29:55 +0000

    studio-1637293473:~/studio{main}$ npx cap sync android [error] Could not

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.ariscbt.com',
   appName: 'ArisCBT',
-  webDir: 'out'
+  webDir: 'out',
+  server: {
+    url: 'http://192.168.1.100:9002', // Change this to your computer's local IP address
+    cleartext: true
+  }
 };
 
 export default config;
